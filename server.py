34123ba4a97222cc66b512c23876c992abdef8a5
Prince Mathew <mailprincemathew@gmail.com>
2019-09-28 16:41:11 +0530

    third

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 import socket
-host = '192.168.43.5'
+host = "192.168.43.4"
 port = 5000
 addr = (host,port)
-#print(addr)
 server_socket = socket.socket()
 server_socket.bind(addr)
-server_socket-listen()
+server_socket.listen(5)
+  
+
+while True: 
+   c, addr = server_socket.accept()
+   data = c.recv(1024).decode()
+   print ('client-> ', str(data)) 
+   data = input('me-> ') 
+   c.send(data.encode())
+c.close() 
